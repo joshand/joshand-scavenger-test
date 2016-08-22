f2ccee854992abc66e580182c45cf83841687bec
joshand <joshand@cisco.com>
2016-08-22 13:10:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
-
+    #return "Hello World!!"
+    r = requests.get("https://1faoejtuza.execute-api.us-east-1.amazonaws.com/prod/gsx-challenge")
+    return r.content
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
